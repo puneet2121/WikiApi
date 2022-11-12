55c62c9987aca83d081722229188156aeacf526d
puneet2121 <bedipuneet21@gmail.com>
2022-11-11 22:45:47 -0700

    check connection with the database

--- app.js
+++ app.js
@@ -5,19 +5,28 @@ const mongoose = require('mongoose');
 
 //middleware
 app.use(bodyParser.urlencoded({extended:true}));
-app.get('/',(req,res) => {
-  res.send('hello')
-});
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
-mongoose.connect("mongodb://localhost:27017/wikiApi", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/wikiDb", {useNewUrlParser: true});
+
+app.get('/articles',(req,res) => {
+  Article.find(function(err,foundArticles){
+    if (!err) {
+      res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+  });
+});
 
 const articleSchema = mongoose.Schema ({
-  title: String,
-  name: String
+  title: String,  
+  content: String
 
 });
 
-const Article = mongoose.model('person',articleSchema);
+const Article = mongoose.model('article',articleSchema);
 
 app.listen(3000,() => {
   console.log('listeinig on 3000')
