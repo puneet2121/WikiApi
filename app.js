f47aa0b1fe5889ae5a360e7b993dd39cf9f99601
puneet2121 <bedipuneet21@gmail.com>
2022-11-12 00:52:24 -0700

    save data with post req

--- app.js
+++ app.js
@@ -4,29 +4,39 @@ const app = express();
 const mongoose = require('mongoose');
 
 //middleware
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended:true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-mongoose.connect("mongodb://localhost:27017/wikiDb", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/newDb", {useNewUrlParser: true});
 
-app.get('/articles',(req,res) => {
-  Article.find(function(err,foundArticles){
-    if (!err) {
-      res.send(foundArticles);
-    } else {
-      res.send(err);
-    }
-  });
-});
-
-const articleSchema = mongoose.Schema ({
+const articleSchema = new mongoose.Schema ({
   title: String,  
   content: String
 
 });
 
 const Article = mongoose.model('article',articleSchema);
+
+app.get('/articles',(req,res) => {
+  Article.find(function(err,foundArticles){
+    if (!err) {
+      res.send(foundArticles);
+    } else {
+      res.json(err);
+    }
+  });
+});
+
+app.post("/articles", (req,res) => {
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  })
+  newArticle.save(function(err) {
+    console.log(err);
+  })
+})
 
 app.listen(3000,() => {
   console.log('listeinig on 3000')
